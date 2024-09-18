b58b016738d0c7efa2710ebdc913b6a954201a12
Floral <49110090+floral-qua-floral@users.noreply.github.com>
2024-09-17 23:52:43 -0400

    Trying to make linear acceleration

--- src/main/java/squeek/quakemovement/MarioClientTravel.java
+++ src/main/java/squeek/quakemovement/MarioClientTravel.java
@@ -1,9 +1,7 @@
 package squeek.quakemovement;
 
-import com.mojang.brigadier.Message;
 import net.minecraft.entity.MovementType;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.Text;
 import net.minecraft.util.math.Vec3d;
 import org.slf4j.Logger;
 
@@ -16,8 +14,12 @@ public class MarioClientTravel {
 		}
 	}
 
+	public static double forwardVel;
+	public static double rightwardVel;
+
 	enum State {
-		DEBUG
+		DEBUG,
+		GROUNDED
 	}
 	private static State marioState = State.DEBUG;
 
@@ -31,39 +33,113 @@ public class MarioClientTravel {
 
 		// Calculate current forwards and sideways velocity
 		Vec3d currentVel = player.getVelocity();
-		double forwardVel = currentVel.x * forwardX + currentVel.z * forwardZ;
-		double rightwardVel = currentVel.x * rightwardX + currentVel.z * rightwardZ;
+		forwardVel = currentVel.x * forwardX + currentVel.z * forwardZ;
+		rightwardVel = currentVel.x * rightwardX + currentVel.z * rightwardZ;
 
 		// Evaluate the direction the player is inputting
-		double intendedForwardVel = movementInput.z;
-		double intendedRightwardVel = movementInput.x;
-		double holdForward = (Math.abs(intendedForwardVel) < 0.1) ? 0.0 : Math.signum(intendedForwardVel);
-		double holdSide = (Math.abs(intendedRightwardVel) < 0.1) ? 0.0 : Math.signum(intendedRightwardVel);
+		Vec3d inputVector = movementInput.multiply(1);
+		if(inputVector.horizontalLengthSquared() > 1) inputVector = inputVector.normalize();
+
+		double forwardInput = (Math.abs(inputVector.z) < 0.1) ? 0.0 : inputVector.z;
+		double rightwardInput = (Math.abs(inputVector.x) < 0.1) ? 0.0 : inputVector.x;
+
+		double intendedForwardVel = 0;
+		double intendedRightwardVel = 0;
+
+		double yVel = currentVel.y;
 
 		// Behave differently depending on Mario's state
 		switch(marioState) {
 			case DEBUG:
-//				accelPlayer(player, movementInput.z * 0.1, movementInput.x * 0.15, 0.5, -0.25, 0.35);
+//				accelPlayer(player, movementInput.z * 0.35, movementInput.x * 0.45, 0.5, -0.25, 0.35);
 ////				LOGGER.info(String.valueOf(player.getVelocity()));
 //				player.setVelocity(movementInput.x, 0, movementInput.z);
 
-				forwardVel = intendedForwardVel;
-				rightwardVel = intendedRightwardVel;
+				intendedForwardVel = forwardInput * 0.5;
+				intendedRightwardVel = rightwardInput * 0.5;
+			break;
+
+			case GROUNDED:
+				double forwardAccel = 0;
+				double rightwardAccel = 0;
+				if(forwardInput > 0) {
+//					forwardVel = approachNumber(forwardVel, forwardInput * 0.1, 1);
+					if (forwardVel <= forwardInput) {
+						// Walk Accel
+						forwardAccel = forwardInput * 0.1;
+					} else {
+						// Overwalk
+						forwardAccel = -0.05;
+					}
+				} else if(forwardInput < 0) {
+					if(forwardVel > 1000) {
+						// Transition to skid!
+					} else if(forwardVel >= forwardInput) {
+						// Backup Accel
+					} else {
+						// Overbackup
+					}
+				} else {
+					// Not moving forward or backwards
+					forwardAccel = 0;
+				}
+
+//				accelPlayer(player, forwardAccel, rightwardAccel, 1, 1, 1);
 			break;
 
 			default:
 				throw new InvalidMarioStateException("Mario state not handled in switch-case!!!");
 		}
 
-		// Calculate new speed
+		// Make relative velocities approach intended relative velocities
+		double deltaForwardVel = 0.005 * Math.signum(intendedForwardVel - forwardVel);
+		if(Math.abs(deltaForwardVel) >= Math.abs(forwardVel - intendedForwardVel))
+			forwardVel = intendedForwardVel;
+		else
+			forwardVel += deltaForwardVel;
+
+		double deltaRightwardVel = 0.1 * Math.signum(intendedRightwardVel - rightwardVel);
+		if(Math.abs(deltaRightwardVel) >= Math.abs(rightwardVel - intendedRightwardVel))
+			rightwardVel = intendedRightwardVel;
+		else
+			rightwardVel += deltaRightwardVel;
+
+		// Calculate new cardinal velocities
 		double newXVel = forwardX * forwardVel + rightwardX * rightwardVel;
 		double newZVel = forwardZ * forwardVel + rightwardZ * rightwardVel;
-		player.setVelocity(newXVel, 0, newZVel);
+		Vec3d newVel = new Vec3d(newXVel, 0, newZVel);
 
+		// But you can't accelerate to any speed that's higher than your highest intended velocity!
+		if(newVel.horizontalLengthSquared() > currentVel.horizontalLengthSquared()) {
+			double speedCap = Math.max(Math.abs(intendedForwardVel), Math.abs(intendedRightwardVel));
+			double speedCapSquared = speedCap * speedCap;
+
+			if(currentVel.horizontalLengthSquared() > speedCapSquared) {
+				// We're already moving faster than we want to be, so how are we accelerating?????
+				newVel = newVel.normalize().multiply(currentVel.horizontalLength());
+				LOGGER.info("Moving faster than we want to but also accelerating??");
+			} else if(newVel.horizontalLengthSquared() > speedCapSquared) {
+				// We're just about to pass our speed cap, so limit our speed to match it
+				newVel = newVel.normalize().multiply(speedCap);
+				LOGGER.info("Matching speed cap");
+			}
+		}
+
+		// Assign and use the new velocity
+		player.setVelocity(newXVel, yVel, newZVel);
 		player.move(MovementType.SELF, player.getVelocity());
-		player.updateLimbs(true);
+		player.updateLimbs(false);
 
 		return true;
+	}
+
+	public static double approachNumber(double start, double delta, double target) {
+		start += delta;
+		if(delta > 0) {
+			if(start > target) return target;
+		} else if(delta < 0 && start < target) return target;
+
+		return start;
 	}
 
 	public static void accelPlayer(PlayerEntity player, double forward, double rightward, double forwardCap, double backwardCap, double sideCap) {
@@ -76,8 +152,8 @@ public class MarioClientTravel {
 
 		// Calculate current forwards and sideways velocity
 		Vec3d currentVel = player.getVelocity();
-		double forwardVel = currentVel.x * forwardX + currentVel.z * forwardZ;
-		double rightwardVel = currentVel.x * rightwardX + currentVel.z * rightwardZ;
+//		double forwardVel = currentVel.x * forwardX + currentVel.z * forwardZ;
+//		double rightwardVel = currentVel.x * rightwardX + currentVel.z * rightwardZ;
 
 		// Apply speed caps
 		if (forward > 0) {
